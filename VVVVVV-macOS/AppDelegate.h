ec27bc54b586c1c26b49a845cf1f06203ad26353
Tom Kidd <tomkidd@gmail.com>
2020-01-10 15:04:59 -0600

    Initial Commit

---
+++ VVVVVV-macOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VVVVVV-macOS
+//
+//  Created by Tom Kidd on 1/10/20.
+//  Copyright © 2020 Tom Kidd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ VVVVVV-macOS/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  VVVVVV-macOS
+//
+//  Created by Tom Kidd on 1/10/20.
+//  Copyright © 2020 Tom Kidd. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
